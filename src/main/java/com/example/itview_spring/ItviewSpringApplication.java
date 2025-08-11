64445496dd5a7ce7fa51311835c1bb5798f892c2
tatoon0 <qudwjd4296@naver.com>
2025-08-11 11:08:17 +0900

    Initialize Spring Boot project

---
+++ src/main/java/com/example/itview_spring/ItviewSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.itview_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItviewSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItviewSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itview_spring/ItviewSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itview_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItviewSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
