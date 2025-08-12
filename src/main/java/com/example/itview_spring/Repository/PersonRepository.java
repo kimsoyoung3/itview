6840d87664f2411c2aafe26326b041672efa7d45
himne79ya <himne79yaya@gmail.com>
2025-08-12 17:20:57 +0900

    인물 관련 컨트롤러, html 작성

---
+++ src/main/java/com/example/itview_spring/Repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package com.example.itview_spring.Repository;
+
+import com.example.itview_spring.Entity.PersonEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository <PersonEntity, Integer> {
+}
