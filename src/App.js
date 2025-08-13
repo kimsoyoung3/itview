d46cdd764fe3099232d44b4fb09a0da3e2c8941d
tatoon0 <qudwjd4296@naver.com>
2025-08-13 10:44:54 +0900

    layout.js import 삭제

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Layout from './components/Layout';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import AppRoutes from './routes/AppRoutes';
