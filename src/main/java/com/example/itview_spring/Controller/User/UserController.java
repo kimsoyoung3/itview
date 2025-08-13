65f042303cdf40f5dd2ddd41a404302bbb9b1900
tatoon0 <qudwjd4296@naver.com>
2025-08-13 11:59:25 +0900

    UserController에 origin 추가

--- src/main/java/com/example/itview_spring/Controller/User/UserController.java
+++ src/main/java/com/example/itview_spring/Controller/User/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/user")
+@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class UserController {
     
     private final RegisterService registerService;
