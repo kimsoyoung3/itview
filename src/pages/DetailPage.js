61d15d7e08dea22b109555bc1e3d1e4548dd15f6
ksy <thduddjrk@gmail.com>
2025-08-20 11:27:26 +0900

    디테일 페이지

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,20 +1,30 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "../App.css"; // CSS 따로 관리
+import "../App.css";
+import {NavLink} from "react-router-dom"; // CSS 따로 관리
 
 const DetailPage = () => {
     return (
-        <div className="detail-banner">
-            <div className="banner-post">
-                <img src="" alt=""/>
+        <div className="detail">
+            <div className="detail-banner">
+                <div className="banner-post">
+                    <img src="/image.jpg" alt=""/>
+                </div>
+
+                <div className="post-dec container">
+                    <ul className="">
+                        <li>영화 제목</li>
+                        <li>영화 설명 칸</li>
+                        <li><span>개봉년도</span> &middot; <span>장르</span> &middot; <span>국가</span></li>
+                        <li><span>영화시간</span >&middot; <span>나이제한</span></li>
+                    </ul>
+                </div>
+
+                <NavLink to="/" className=""></NavLink>
+
             </div>
 
-            <ul className="post-dec">
-                <li>영화 제목</li>
-                <li>영화 설명 칸</li>
-                <li><span>개봉년도</span>&middot;<span>장르</span>&middot;<span>국가</span></li>
-                <li></li>
-            </ul>
+            <div className="detail-content container"></div>
         </div>
     )
 }
