c1883ec21acdf97b02184c51489cabb02d1afdad
tatoon0 <qudwjd4296@naver.com>
2025-08-21 09:40:07 +0900

    크레딧 api 추가

--- src/API/ContentApi.js
+++ src/API/ContentApi.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
 export const getContentDetail = (id) => axios.get(`http://localhost:8080/api/content/${id}`, {withCredentials: true});
+export const getContentCredit = (page) => axios.get(`http://localhost:8080/api/content/credit?page=${page}`, {withCredentials: true});
