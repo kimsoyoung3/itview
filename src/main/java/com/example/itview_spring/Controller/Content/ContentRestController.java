77217d57e20518c8894b0e5b339b63bf5ca6c1e6
tatoon0 <qudwjd4296@naver.com>
2025-08-21 10:25:52 +0900

    안쓰는 import 정리

--- src/main/java/com/example/itview_spring/Controller/Content/ContentRestController.java
+++ src/main/java/com/example/itview_spring/Controller/Content/ContentRestController.java
@@ -1,12 +1,9 @@
 package com.example.itview_spring.Controller.Content;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.method.P;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
