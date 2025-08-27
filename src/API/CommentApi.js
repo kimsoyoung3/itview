f4beec3e60477fc3dbe6d981e25f86a9960070ac
tatoon0 <qudwjd4296@naver.com>
2025-08-27 17:49:12 +0900

    Add getCommentAndContent API function

--- src/API/CommentApi.js
+++ src/API/CommentApi.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+export const getCommentAndContent = (commentId) => axios.get(`http://localhost:8080/api/comment/${commentId}`, {withCredentials: true});
 export const likeComment = (commentId) => axios.post(`http://localhost:8080/api/comment/${commentId}/like`, {}, {withCredentials: true});
 export const unlikeComment = (commentId) => axios.delete(`http://localhost:8080/api/comment/${commentId}/like`, {withCredentials: true});
 export const postReply = (commentId, data) => axios.post(`http://localhost:8080/api/comment/${commentId}/reply`, data, {withCredentials: true});
