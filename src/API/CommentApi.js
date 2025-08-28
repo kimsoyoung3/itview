d7e95d9496eca35326f8150cf6513d7b9220fb90
tatoon0 <qudwjd4296@naver.com>
2025-08-28 10:23:36 +0900

    Add getCommentRepliesPaged API function

--- src/API/CommentApi.js
+++ src/API/CommentApi.js
@@ -4,3 +4,4 @@ export const getCommentAndContent = (commentId) => axios.get(`http://localhost:8
 export const likeComment = (commentId) => axios.post(`http://localhost:8080/api/comment/${commentId}/like`, {}, {withCredentials: true});
 export const unlikeComment = (commentId) => axios.delete(`http://localhost:8080/api/comment/${commentId}/like`, {withCredentials: true});
 export const postReply = (commentId, data) => axios.post(`http://localhost:8080/api/comment/${commentId}/reply`, data, {withCredentials: true});
+export const getCommentRepliesPaged = (commentId, page) => axios.get(`http://localhost:8080/api/comment/${commentId}/reply?page=${page}`, {withCredentials: true});
