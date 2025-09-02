618bb87d807f0df68dff119c07fd4d94f5b31711
ksy <thduddjrk@gmail.com>
2025-09-02 09:35:33 +0900

    인물 디테일 페이지 수정

--- src/pages/PersonDetailPage/PersonDetailPage.js
+++ src/pages/PersonDetailPage/PersonDetailPage.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from "react";
 import "./PersonDetailPage.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { getPersonInfo, getPersonWorkDomains, getPersonWorks } from "../../API/PersonApi";
-import { useParams } from "react-router-dom";
+import {NavLink, useParams} from "react-router-dom";
 
 
 const PersonDetailPage = ({userInfo, openLogin}) => {
@@ -72,34 +72,120 @@ const PersonDetailPage = ({userInfo, openLogin}) => {
         }
     }
 
+    const headerHeight = 94; // 헤더 높이(px)
+
+    const handleTabClick = (contentType, department = null) => {
+        const idToScroll = department ? `${contentType}-${department}` : contentType;
+        const element = document.getElementById(idToScroll);
+        if (element) {
+            const top = element.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+            window.scrollTo({ top, behavior: 'smooth' });
+            setActiveTab(department ? `${contentType}-${department}` : contentType);
+        }
+    };
+
+
+    const [activeTab, setActiveTab] = useState(null);
+
+    /*외부서비스 로고*/
+    const serviceLogos = {
+        NETFLIX: '/externalLogo/netflix.png',
+        DISNEY_PLUS: '/externalLogo/disney.png',
+        WAVE: '/externalLogo/wave.png',
+        WATCHA: '/externalLogo/watcha.png',
+        TVING: '/externalLogo/tving.png',
+        TVN: '/externalLogo/tvn.png',
+        APPLE_TV_PLUS: '/externalLogo/apple-tv-plus.png',
+
+        ALADIN: '/externalLogo/aladin.png',
+        YES24: '/externalLogo/yes24.png',
+        KYOBO: '/externalLogo/kyobo.png',
+
+        NAVER: '/externalLogo/naverWebtoon.png',
+        KAKAO: '/externalLogo/kakaoWebtoon.png',
+    };
+
     return(
         <div className="person-detail-page container">
              <section className="person-detail-page-profile">
                 <div className="person-detail-page-profile-img">
-                    <img src={personInfo?.profile ? personInfo?.profile : "/user.png" } alt=""/>
+                    <NavLink><img src={personInfo?.profile ? personInfo?.profile : "/user.png" } alt=""/></NavLink>
                 </div>
                 <div className="person-detail-page-profile-info">
                     <h4>{personInfo?.name}</h4>
                     <p>{personInfo?.job}</p>
                 </div>
                  <div className="person-detail-page-profile-like">
-                     <hr/>
                      <div><button><i className="bi bi-hand-thumbs-up"/> 좋아요 {personInfo?.likeCount}명이 이 인물을 좋아합니다.</button></div>
-                     <hr/>
                  </div>
             </section>
 
             <section className="person-detail-page-content">
-                {workInfo && Object.entries(workInfo).map(([contentType, departments]) => (
-                    <div key={contentType} className="person-detail-page-content-domain">
-                        <h5>{contentType}</h5>
-                        <ul>
-                            {Object.entries(departments).map(([department, items]) => (
-                                <li key={department}>{department} {workInfo[contentType][department].page.number+1 < workInfo[contentType][department].page.totalPages ? <button onClick={() => handleClickMore(contentType, department, workInfo[contentType][department].page.number+2)}>더보기</button> : null} {items.content.map(item => (item.title))}</li>
-                            ))}
-                        </ul>
-                    </div>
-                ))}
+
+                <div>
+                    {workInfo && Object.entries(workInfo).map(([contentType, departments]) => (
+                        <div key={contentType} id={contentType} className="person-detail-page-content-domain">
+                            <h5>{contentType}</h5>
+                            <div className="person-detail-page-content-domain-wrap">
+                                {Object.entries(departments).map(([department, items]) => (
+                                    <div key={department}>
+                                        <p id={`${contentType}-${department}`}>{department}</p>
+                                        <div className="person-detail-page-content-domain-title">
+                                            <div></div>
+                                            <div></div>
+                                            <div>제목</div>
+                                            <div>역할</div>
+                                            <div>평가</div>
+                                            <div>감상서비스</div>
+                                        </div>
+                                        {items.content.map(item =>
+                                            <div className="person-detail-page-content-domain-list">
+                                                <div className="domain-list-date">{(new Date(item.releaseDate).getFullYear())}</div>
+                                                <div className="domain-list-image"><img src={item.poster} alt=""/></div>
+                                                <div className="domain-list-title">{item.title}</div>
+                                                <div className="domain-list-role">{item.role}</div>
+                                                {item.ratingAvg && (
+                                                    <div className="domain-list-ratingAvg">평균 {item.ratingAvg}</div>
+                                                )}
+                                            </div>
+                                        )}
+                                        <div className="person-detail-page-content-domain-btn">{workInfo[contentType][department].page.number+1 < workInfo[contentType][department].page.totalPages ? <button onClick={() => handleClickMore(contentType, department, workInfo[contentType][department].page.number+2)}>더보기</button> : null} </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+
+                <div className="person-detail-page-content-tab">
+                    <ul>
+                        {workInfo && Object.entries(workInfo).map(([contentType, departments]) => (
+                            <li key={contentType}>
+                                <div>
+                                    <button
+                                        className={activeTab === contentType ? 'active' : ''}
+                                        onClick={() => handleTabClick(contentType)}
+                                    >
+                                        {contentType}
+                                    </button>
+                                </div>
+                                <ul>
+                                    {Object.keys(departments).map((department) => (
+                                        <li key={department}>
+                                            <button
+                                                className={activeTab === `${contentType}-${department}` ? 'active' : ''}
+                                                onClick={() => handleTabClick(contentType, department)}
+                                            >
+                                                {department}
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </section>
         </div>
     )
