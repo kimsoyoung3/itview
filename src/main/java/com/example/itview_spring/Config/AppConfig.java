2082eef9ea424be136447bd71ee66328c1a817be
tatoon0 <qudwjd4296@naver.com>
2025-09-02 14:08:44 +0900

    s3업로더 수정

--- src/main/java/com/example/itview_spring/Config/AppConfig.java
+++ src/main/java/com/example/itview_spring/Config/AppConfig.java
@@ -6,17 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
-import com.example.itview_spring.Util.S3Uploader;
-
 @Configuration
 @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class AppConfig {
 
-    @Bean
-    public S3Uploader s3Uploader() {
-        return new S3Uploader();
-    }
-    
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
