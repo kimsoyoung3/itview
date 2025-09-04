d77fde727b17ac43178dada4fb96ea8b893cea45
tatoon0 <qudwjd4296@naver.com>
2025-09-04 11:45:53 +0900

    유저 컨텐츠 평점 링크 추가

--- src/pages/UserRatingPage/UserRatingPage.js
+++ src/pages/UserRatingPage/UserRatingPage.js
@@ -36,11 +36,11 @@ function UserRatingPage({ userInfo, openLogin }) {
             <div className="user-rating-page-wrap">
                 <h1>평가</h1>
                 <ul className="rating-list">
-                    <li><NavLink>영화 <span>{userRatingCount?.movie}</span></NavLink></li>
-                    <li><NavLink>시리즈 <span>{userRatingCount?.series}</span></NavLink></li>
-                    <li><NavLink>책 <span>{userRatingCount?.book}</span></NavLink></li>
-                    <li><NavLink>웹툰 <span>{userRatingCount?.webtoon}</span></NavLink></li>
-                    <li><NavLink>음반 <span>{userRatingCount?.record}</span></NavLink></li>
+                    <li><NavLink to={`/user/${id}/content/movie/rating`}>영화 <span>{userRatingCount?.movie}</span></NavLink></li>
+                    <li><NavLink to={`/user/${id}/content/series/rating`}>시리즈 <span>{userRatingCount?.series}</span></NavLink></li>
+                    <li><NavLink to={`/user/${id}/content/book/rating`}>책 <span>{userRatingCount?.book}</span></NavLink></li>
+                    <li><NavLink to={`/user/${id}/content/webtoon/rating`}>웹툰 <span>{userRatingCount?.webtoon}</span></NavLink></li>
+                    <li><NavLink to={`/user/${id}/content/record/rating`}>음반 <span>{userRatingCount?.record}</span></NavLink></li>
                 </ul>
             </div>
         </div>
