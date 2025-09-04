2cea22b15c178b37c610468798d76feb10b8a6ef
tatoon0 <qudwjd4296@naver.com>
2025-09-04 16:35:50 +0900

    별점에 등록 시간 추가

--- src/main/java/com/example/itview_spring/Entity/RatingEntity.java
+++ src/main/java/com/example/itview_spring/Entity/RatingEntity.java
@@ -2,8 +2,15 @@ package com.example.itview_spring.Entity;
 
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +25,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class RatingEntity {
     
     @Id
@@ -37,4 +45,9 @@ public class RatingEntity {
     // 평점 (1~10)
     @Column(nullable = false)
     private Integer score;
+
+    // 평점 남긴 시간
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
 }
