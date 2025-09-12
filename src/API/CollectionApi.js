0cd0e314dfd755afbcb8cf8e376e879c7c5c8619
tatoon0 <qudwjd4296@naver.com>
2025-09-12 12:43:57 +0900

    컬렉션 상세 조회 api 추가 및 데이터 fetch

--- src/API/CollectionApi.js
+++ src/API/CollectionApi.js
@@ -1,3 +1,4 @@
 import axios from "axios";
 
 export const CollectionCreate = async (data) => axios.post(`${process.env.REACT_APP_API_URL}/collection`, data, {withCredentials: true});
+export const getCollectionDetail = (id) => axios.get(`${process.env.REACT_APP_API_URL}/collection/${id}`, {withCredentials: true});
--- src/pages/UserCollectionDetailPage/UserCollectionDetailPage.js
+++ src/pages/UserCollectionDetailPage/UserCollectionDetailPage.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getCollectionDetail } from "../../API/CollectionApi";
+import NotFound from "../NotFound/NotFound";
+import CollectionCard from "../../components/CollectionCard/CollectionCard";
 
 const UserCollectionDetailPage = () => {
+    const { id } = useParams();
+    const [collection, setCollection] = useState({});
+    const [notFound, setNotFound] = useState(false);
 
-    return(
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await getCollectionDetail(id);
+                setCollection(res.data);
+            } catch (e) {
+                setNotFound(true);
+            }
+        };
+        fetchData();
+    }, [id]);
+
+    useEffect(() => {
+        console.log(collection);
+    }, [collection]);
+
+    return(notFound ? <NotFound /> :
         <div className="user-collection-detail-page">
-            <div className="user-collection-detail-page-wrap"></div>
+            <div className="user-collection-detail-page-wrap">
+                <h1>{collection?.title}</h1>
+                <div className="user-collection-detail-page-wrap-content">
+                    {collection?.content?.map(item => (
+                        <CollectionCard key={item.id} collectionData={item} />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 
