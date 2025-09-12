b1032a79e35f11572a3bb21e0158c17464b6b5ff
ksy <thduddjrk@gmail.com>
2025-09-12 15:19:53 +0900

    컬렉션 디테일 페이지 버튼들까지

--- src/pages/UserCollectionDetailPage/UserCollectionDetailPage.js
+++ src/pages/UserCollectionDetailPage/UserCollectionDetailPage.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getCollectionDetail } from "../../API/CollectionApi";
 import NotFound from "../NotFound/NotFound";
-import CollectionCard from "../../components/CollectionCard/CollectionCard";
+import "./UserCollectionDetailPage.css"
+import {toast} from "react-toastify";
 
 const UserCollectionDetailPage = () => {
     const { id } = useParams();
     const [collection, setCollection] = useState({});
     const [notFound, setNotFound] = useState(false);
+
+    const [edit, setEdit] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -26,14 +29,86 @@ const UserCollectionDetailPage = () => {
     }, [collection]);
 
     return(notFound ? <NotFound /> :
-        <div className="user-collection-detail-page">
+        <div className="user-collection-detail-page container">
             <div className="user-collection-detail-page-wrap">
-                <h1>{collection?.title}</h1>
-                <div className="user-collection-detail-page-wrap-content">
-                    {collection?.content?.map(item => (
-                        <CollectionCard key={item.id} collectionData={item} />
+                {/* 썸네일 이미지와 그림자 */}
+                <section className="user-collection-detail-bg">
+
+                    {collection?.poster?.slice(0, 5).map((poster, index, arr) => (
+                        <div
+                            key={index}
+                            className="user-collection-detail-stacked-poster-wrapper"
+                            style={{
+                                left: `${index * 15}%`, // 겹치는 위치 조정
+                                zIndex: arr.length - index,
+                            }}
+                        >
+                            <img src={poster} alt={`포스터 ${index}`} />
+                        </div>
                     ))}
-                </div>
+
+                    <div className="user-collection-detail-bg-shadow"></div>
+
+                    {/* 사용자 프로필 */}
+                    <div className="user-collection-detail-profile">
+                        <div className="user-collection-detail-profile-image">
+                            <img
+                                src={collection?.user?.profile ? collection.user.profile : '/user.png'}
+                                alt="사용자 프로필"
+                            />
+                        </div>
+                        <p>{collection?.user?.nickname}</p>
+                    </div>
+
+                    {/*수정&삭제 버튼*/}
+                    <div className="user-collection-detail-edit-box">
+                        {edit && (
+                            <div>
+                                <button>수정</button>
+                                <button>삭제</button>
+                            </div>
+                        )}
+
+                        <button onClick={() => setEdit(prev => !prev)} className="user-collection-detail-edit-btn">
+                            <i className="bi bi-three-dots"></i>
+                        </button>
+                    </div>
+
+                </section>
+
+                {/*제목 및 좋아요, 댓글, 업데이트시간*/}
+                <section className="user-collection-detail-info">
+                    <h1>{collection?.title}</h1>
+                    <p>
+                        <span>좋아요 {collection?.likeCount}</span>
+                        <span> &#124; 댓글 {collection?.replyCount}</span>
+                        <span> &#124; {collection?.updatedAt?.substring(0, 10)}</span>
+                    </p>
+                </section>
+
+                <section className="user-collection-detail-btn-list">
+                    <div className="user-collection-detail-btn-list-wrap">
+                        <div className="user-collection-detail-btn-list-content">
+                            <button>
+                                <i className="bi bi-heart"/>
+                                <span>좋아요</span>
+                            </button>
+                        </div>
+                        <div className="user-collection-detail-btn-list-content">
+                            <button>
+                                <i className="bi bi-chat-square"/>
+                                <span>댓글</span>
+                            </button>
+                        </div>
+                        <div className="user-collection-detail-btn-list-content">
+                            <button>
+                                <i className="bi bi-share"/>
+                                <span>공유</span>
+                            </button>
+                        </div>
+                    </div>
+                </section>
+
             </div>
         </div>
     )
