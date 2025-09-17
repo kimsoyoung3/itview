dcabeb11c5c9ece394c46ac6f576793ec08439cd
ksy <thduddjrk@gmail.com>
2025-09-17 09:22:35 +0900

    s

--- src/main/java/com/example/itview_spring/Config/WebConfig.java
+++ src/main/java/com/example/itview_spring/Config/WebConfig.java
@@ -17,8 +17,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 경로 허용
                 .allowedOrigins(
                     "http://localhost:3000",
-                    "http://192.168.100.43:3000"
-                )
+                    "http://192.168.100.43:3000", "http://192.168.100.121:3000"
+                        )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true)
                 .allowedHeaders("*");
