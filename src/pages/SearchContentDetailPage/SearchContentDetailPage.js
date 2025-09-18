2c0fec758fb12114daf59118512c3e29e8287925
ksy <thduddjrk@gmail.com>
2025-09-18 10:52:04 +0900

    검색 컨텐츠 더보기 페이지

--- src/pages/SearchContentDetailPage/SearchContentDetailPage.js
+++ src/pages/SearchContentDetailPage/SearchContentDetailPage.js
@@ -3,6 +3,7 @@ import "./SearchContentDetailPage.css"
 import { useSearchParams } from "react-router-dom";
 import NotFound from "../NotFound/NotFound";
 import { searchContentsDetail } from "../../API/SearchApi";
+import SearchContentEach from "../../components/SearchContentEach/SearchContentEach";
 
 const SearchContentDetailPage = () => {
     const [notFound, setNotFound] = useState(false);
@@ -29,6 +30,14 @@ const SearchContentDetailPage = () => {
             console.log(data);
     }, [data]);
 
+    const domainNameMap = {
+        "movie" : "영화",
+        "series" : "시리즈",
+        "book" : "책",
+        "webtoon" : "웹툰",
+        "record" : "음반"
+    }
+
     return (notFound ? <NotFound /> :
         <div className="search-content-detail-page">
             <div className="search-result">
@@ -36,11 +45,18 @@ const SearchContentDetailPage = () => {
             </div>
 
             <div className="search-content-detail-page-wrap container">
-                <p>영화</p>
+                <p>{domainNameMap[type]}</p>
 
                 <div className="search-content-list-wrap">
-
+                    {data?.content.map(item =>
+                        <div className="search-content-list">
+                            <SearchContentEach key={item.id} searchData={item}/>
+                        </div>
+                    )}
                 </div>
+
+                <div className="search-content-more-btn"><button>더보기</button></div>
+
             </div>
         </div>
     );
