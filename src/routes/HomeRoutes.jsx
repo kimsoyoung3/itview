97006868040b32b3b7047e372e9c53918b6267b8
tatoon0 <qudwjd4296@naver.com>
2025-09-24 11:43:12 +0900

    웹툰 도메인 api 추가

--- src/routes/HomeRoutes.jsx
+++ src/routes/HomeRoutes.jsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home/Home';
 import Movie from '../pages/Home/Movie/Movie';
 import Series from '../pages/Home/Series/Series';
 import Book from '../pages/Home/Book/Book';
+import Webtoon from '../pages/Home/Webtoon/Webtoon';
 
 function HomeRoutes({ userInfo, openLogin }) {
   return (
@@ -12,6 +13,7 @@ function HomeRoutes({ userInfo, openLogin }) {
       <Route path="/movie" element={<Movie />} />
       <Route path="/series" element={<Series />} />
       <Route path="/book" element={<Book />} />
+      <Route path="/webtoon" element={<Webtoon />} />
     </Routes>
   )
 }
