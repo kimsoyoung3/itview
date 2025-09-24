f7b015e1abe52876bb0672882109ccfd530804c5
tatoon0 <qudwjd4296@naver.com>
2025-09-24 11:29:57 +0900

    api 주소 수정

--- src/API/HomeApi.js
+++ src/API/HomeApi.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 export const getHomeContents = (contentType, page) => axios.get(`${process.env.REACT_APP_API_URL}/api/home?contentType=${contentType}&page=${page}`, { withCredentials: true });
-export const getGenresByContentType = (contentType) => axios.get(`${process.env.REACT_APP_API_URL}/api/home/${contentType}/domain`, { withCredentials: true });
+export const getGenresByContentType = (contentType) => axios.get(`${process.env.REACT_APP_API_URL}/api/home/${contentType}/genre`, { withCredentials: true });
