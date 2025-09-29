78da8839a124c188483a78f9ade27feff29dcef2
tatoon0 <qudwjd4296@naver.com>
2025-09-29 12:43:47 +0900

    사용자 컬렉션 더보기 기능 추가

--- src/pages/UserCollectionPage/UserCollectionPage.js
+++ src/pages/UserCollectionPage/UserCollectionPage.js
@@ -1,9 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./UserCollectionPage.css"
 import NotFound from "../NotFound/NotFound";
 import { NavLink, useParams } from "react-router-dom";
 import { getUserCollection } from "../../API/UserApi";
 import CollectionCard from "../../components/CollectionCard/CollectionCard";
+import { toast } from "react-toastify";
 
 const UserCollectionPage = ({userInfo}) => {
     const [notFound, setNotFound] = useState(false);
@@ -28,6 +29,40 @@ const UserCollectionPage = ({userInfo}) => {
         console.log(collections);
     }, [collections]);
 
+    const moreRef = useRef();
+
+    const loadMore = async () => {
+        console.log("load more");
+        if (collections.page.number + 1 < collections.page.totalPages) {
+            console.log("load more api");
+            try {
+                const res = await getUserCollection(id, collections.page.number + 2);
+                setCollections({
+                    content: [...collections.content, ...res.data.content],
+                    page: res.data.page,
+                });
+            } catch (e) {
+                toast("컬렉션을 불러오지 못했습니다.");
+            }
+        }
+    };
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            if (entries[0].isIntersecting) {
+                loadMore();
+            }
+        }, { threshold: 0.1 });
+        if (moreRef.current) {
+            observer.observe(moreRef.current);
+        }
+        return () => {
+            if (moreRef.current) {
+                observer.unobserve(moreRef.current);
+            }
+        };
+    }, [collections]);
+
     return( notFound ? <NotFound /> :
         <div className="user-collection-page">
             <div className="user-collection-page-wrap container">
@@ -41,10 +76,7 @@ const UserCollectionPage = ({userInfo}) => {
                         {collections?.content.map(item =>
                             <CollectionCard key={item.id} collectionData={item}/>
                         )}
-
-                        <div className="user-collection-list-btn">
-                            <button>더보기</button>
-                        </div>
+                        <div ref={moreRef} hidden={collections.page.number + 1 >= collections.page.totalPages} style={{ height: '20px' }}></div>
                     </div>
                 ) : (
                     <p className="empty-message">컬렉션이 없습니다 :)</p>
