aaae02205a92f0344519cf75d71e06bf9ab22db8
tatoon0 <qudwjd4296@naver.com>
2025-10-01 10:41:54 +0900

    친구 소식 api 추가

--- src/main/java/com/example/itview_spring/Repository/ActivityLogRepository.java
+++ src/main/java/com/example/itview_spring/Repository/ActivityLogRepository.java
@@ -1,6 +1,10 @@
 package com.example.itview_spring.Repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.itview_spring.Entity.ActivityLogEntity;
 import com.example.itview_spring.Constant.ActivityLogType;
@@ -10,4 +14,23 @@ import com.example.itview_spring.Constant.ActivityLogType;
 public interface ActivityLogRepository extends JpaRepository<ActivityLogEntity, Integer> {
     ActivityLogEntity findByReferenceIdAndType(Integer referenceId, ActivityLogType type);
     void deleteByReferenceIdAndType(Integer referenceId, ActivityLogType type);
+
+    @Query("""
+        SELECT new com.example.itview_spring.Entity.ActivityLogEntity(
+            a.id,
+            a.user,
+            a.type,
+            a.referenceId,
+            a.timestamp,
+            a.isUpdate
+        )
+        FROM ActivityLogEntity a
+        WHERE a.user.id IN (
+            SELECT f.following.id
+            FROM FollowEntity f
+            WHERE f.follower.id = :userId
+        )
+        ORDER BY a.timestamp DESC
+    """)
+    Page<ActivityLogEntity> findFriendActivities(@Param("userId") Integer userId, Pageable pageable);
 }
